af8bdfb7eb48fdff139875b7d49c95377d5cf3fe
DerTeufel <dominik-kassel@gmx.de>
2023-01-15 16:10:06 +0100

    don't force enable adb on user builds

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt8768.rc
 LOCAL_MODULE_TAGS  := optional
@@ -14,3 +15,12 @@ LOCAL_SRC_FILES     := adbenable.sh
 LOCAL_MODULE_SUFFIX := .sh
 LOCAL_MODULE_CLASS  := EXECUTABLES
 include $(BUILD_PREBUILT)
+else
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mt8768.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/empty.txt
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init/hw
+include $(BUILD_PREBUILT)
+endif
