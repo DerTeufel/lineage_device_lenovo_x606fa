ecfe4280a0e7e4968534518788e10c4cc6cff5c8
DerTeufel <dominik-kassel@gmx.de>
2023-01-18 21:47:32 +0100

    use some android go gapps
    2 gb of ram isn't too much nowadays, and the device seems to be much more responsive without gapps...

--- lineage_x606fa.mk
+++ lineage_x606fa.mk
@@ -4,6 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_release.mk)
 $(call inherit-product, device/lenovo/x606fa/device.mk)
 
 # Gapps
+TARGET_USE_GO_GAPPS := true
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 
 PRODUCT_BRAND := lenovo
