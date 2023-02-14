cee52b00ed52961e7776b4d0149adaa4ad48290e
DerTeufel <dominik-kassel@gmx.de>
2023-02-14 19:41:36 +0100

    fix wrong ifdef (user/userdebug builds)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt8768.rc
 LOCAL_MODULE_TAGS  := optional
@@ -9,6 +9,7 @@ LOCAL_SRC_FILES    := etc/init.mt8768.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := adb.rc
 LOCAL_MODULE_TAGS  := optional
@@ -25,7 +26,7 @@ LOCAL_MODULE_CLASS  := EXECUTABLES
 include $(BUILD_PREBUILT)
 else
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mt8768.rc
+LOCAL_MODULE       := adb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/empty.txt
